97c24694a0bb6a54cbce9533d4429004eb6742cb
ParasUOIT <55296232+ParasUOIT@users.noreply.github.com>
2020-03-26 22:44:57 -0400

    Create UserListController.java

---
+++ src/Client/UserListController.java
@@ -0,0 +1,30 @@
+package client;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+
+public class channelController {
+
+    @FXML
+    private ListView<?> channelListview;
+
+    @FXML
+    private ListView<?> memberListview;
+
+    @FXML
+    private Button btAdd;
+
+    @FXML
+    private Button btRemove;
+
+    @FXML
+    void add(ActionEvent event) {
+
+    }
+
+    @FXML
+    void remove(ActionEvent event) {
+
+    }
+
+}
